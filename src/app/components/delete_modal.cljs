710d9faa8742642f89820c866c5d78e56f48ecc0
cghael <a.p.gorobets@gmail.com>
2022-11-29 22:47:13 +0400

    UI is ready!

---
+++ src/app/components/delete_modal.cljs
@@ -0,0 +1,18 @@
+(ns app.components.delete-modal 
+  (:require [app.state :as state]))
+
+
+(defn delete-modal
+  [{:keys [*patient-delete toggle-delete-window delete-patient]}]
+  (let [current (str (:name @state/*activ-id) " " (:s-name @state/*activ-id))]
+    [:div.patient-window__underlay (when @*patient-delete {:class "active"})
+     [:div.patient-delete
+      [:div.patient-delete__body
+       (str "Do you really want to remove " current " from the list?")]
+      [:div.patient-delete__footer
+       [:button.window-btn {:style {:color "#861414"}
+                            :on-click #(toggle-delete-window false)}
+        (str \u2717 " No")]
+       [:button.window-btn {:style {:color "#14861c"}
+                            :on-click #(delete-patient)}
+        (str \u2713 " Yes")]]]]))
--- src/app/components/menu.cljs
+++ src/app/components/menu.cljs
@@ -1,6 +1,7 @@
 (ns app.components.menu
   (:require [app.state :as state]
             [reagent.core :as r]
+            [app.components.delete-modal :refer [delete-modal]]
             [app.components.patient-editor :refer [patient-editor]]
             [clojure.string :as string]))
 
@@ -8,6 +9,7 @@
 (defn menu
   []
   (let [*patient-window (r/atom false)
+        *patient-delete (r/atom false)
         initial-values {:id nil
                         :name ""
                         :s-name ""
@@ -19,6 +21,7 @@
         toggle-patient-window (fn [{:keys [active patient]}]
                                 (reset! *patient-window active)
                                 (reset! *patient-values patient))
+        toggle-delete-window #(reset! *patient-delete %)
         save-patient (fn [{:keys [id name s-name sex birth adress oms-number]}]
                        (let [id-number (:patient-number @state/*patients)
                              id (or id (str "p-" (inc id-number)))]
@@ -31,7 +34,12 @@
                                                                         :oms-number (string/trim oms-number)})
                          (swap! state/*patients update :patient-number inc)
                          (toggle-patient-window {:active false
-                                                 :patient initial-values})))]
+                                                 :patient initial-values})))
+        delete-patient (fn []
+                         (swap! state/*patients update-in [:content] dissoc (:id @state/*activ-id))
+                         (swap! state/*patients update :patient-number dec)
+                         (reset! state/*activ-id nil)
+                         (toggle-delete-window false))]
     (fn []
       [:div.menu
        [:div.menu-flex
@@ -40,19 +48,25 @@
           {:on-click #(toggle-patient-window {:active true
                                               :patient initial-values})}
           "Create"]
-         [:button.menu-btn (when @state/*activ-id 
+         [:button.menu-btn (when @state/*activ-id
                              {:class "active"
                               :on-click #(toggle-patient-window {:active true
                                                                  :patient @state/*activ-id})})
-          "Update"]
-         [:button.menu-btn (when @state/*activ-id {:class "active"})
+          "Edit"]
+         [:button.menu-btn (when @state/*activ-id
+                             {:class "active"
+                              :on-click #(toggle-delete-window true)})
           "Delete"]]
-        [patient-editor {:status "Add new patient"
+        [patient-editor {:status "Patient's card"
                          :*patient-window *patient-window
                          :*patient-values *patient-values
                          :initial-values initial-values
                          :toggle-patient-window toggle-patient-window
                          :save-patient save-patient}]
+        (.log js/console @*patient-delete)
+        [delete-modal {:*patient-delete *patient-delete
+                       :toggle-delete-window toggle-delete-window
+                       :delete-patient delete-patient}]
         [:div.menu-logo
          [:img.img__bottom {:src "img/footer-logo.png"
                             :alt "Patient list logo"}]]]])))
